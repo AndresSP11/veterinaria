5af8e35cf4bd57eb4843c61ecfd5c6e7c7a81b27
Andres <andres.salazar.p@uni.pe>
2023-07-27 17:36:18 -0500

    culminado

--- admin-pacientes/src/components/Alerta.vue
+++ admin-pacientes/src/components/Alerta.vue
@@ -1,18 +1,21 @@
 <script setup>
-    import {computed} from vue
+    import {computed} from 'vue'
     const props= defineProps({
         alerta:{
             type:Object,
             required:true
         }
     })
-    const isError= computed(()=>{
-        return "w-full rounded-md bg-red-600 text-yellow-50 font-bold h-12 text-center p-3 mb-3";
-    })
+    /* const isError= computed(()=>{
+        if(props.alerta.tipo=="error"){
+            return "bg-red-600"
+        }else{
+            return "bg-green-400"
+        }
+    }) */
 </script>
 
 <template>
     <div 
-    :class="isError"
-    class="w-full rounded-md bg-red-600 text-yellow-50 font-bold h-12 text-center p-3 mb-3">{{ alerta.mensaje }}</div>
+    class="w-full rounded-md text-yellow-50 font-bold h-12 text-center p-3 mb-3">{{ alerta.mensaje }}</div>
 </template>
